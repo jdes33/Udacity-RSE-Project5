19ae75590747675b3665353d0db3111e8ed47f1f
Jason <j.desouza206@gmail.com>
2021-09-02 21:03:09 +0100

    Add my world and make test_slam file use it

--- scripts/test_slam.h
+++ scripts/test_slam.h
@@ -1,8 +1,12 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
-sleep 20
-xterm  -e  " rosrun gmapping slam_gmapping " & 
+#xterm  -e  "  roslaunch turtlebot_gazebo turtlebot_world.launch " &
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/PROJECT-5-HOME-SERVICE-ROBOT/catkin_ws/src/map/myworld.world" &
+
+sleep 25
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+#xterm  -e  "  rosrun gmapping slam_gmapping " & 
 sleep 20
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 20
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
+xterm -e "  roslaunch turtlebot_teleop keyboard_teleop.launch "
